83d830bbec01fbd5a027d2fe6f0fb431b7f91dfe
ckingbailey <ckingbailey@gmail.com>
2023-08-26 20:38:53 -0700

    Feat: load committees and elections  tables (#1)
    * add .gitignore
    
    * add committee model
    
    * add main.py to run everything
    
    * add pycache to .gitignore
    
    * recase model files
    
    * commit model __init__
    
    * gitignore env
    
    * add dev requirements
    
    * insert elections table
    
    * add .vscode dir to gitignore
    
    * get committee elections and load committess into pg

---
+++ main.py
@@ -0,0 +1,43 @@
+""" main, to run everything """
+import json
+from sqlalchemy import create_engine
+from model.committee import CommitteeCollection
+from model.election import ElectionCollection
+
+def get_last_status(status_list):
+    """
+    Return a tuple of index, status_item
+    for max value of status_item['startDate']
+    """
+
+def unique_statuses(filers):
+    """ What are the unique values for status? """
+    return set(
+        s['status'] for f in filers
+        for s in f['statusList']
+    )
+
+def main():
+    """ Do everyting """
+    engine = create_engine('postgresql+psycopg2://localhost/disclosure-backend-v2', echo=True)
+
+    with open('data/elections.json', encoding='utf8') as f:
+        elections_json = json.loads(f.read())
+
+    elections = ElectionCollection(elections_json).df
+
+    with open('data/filers.json', encoding='utf8') as f:
+        filers = json.loads(f.read())
+
+    committees = CommitteeCollection.from_filers(filers, elections)
+
+    with engine.connect() as conn:
+        common_opts = {
+            'index_label': 'id',
+            'if_exists': 'replace'
+        }
+        elections.to_sql('elections', conn, **common_opts)
+        committees.df.to_sql('committees', conn, **common_opts)
+
+if __name__ == '__main__':
+    main()
---
+++ model/committee.py
@@ -0,0 +1,32 @@
+""" This is the Committee model """
+from typing import List
+import pandas as pd
+from . import base
+
+class CommitteeCollection(base.BaseModel):
+    """ A collection of committees """
+    def __init__(self, data:List[dict]):
+        super().__init__(data)
+        self._dtypes = 'string'
+
+    @classmethod
+    def from_filers(cls, filer_records:List[dict], elections:pd.DataFrame):
+        """ Reshape NetFile filer records """
+        return cls([
+            {
+                'Ballot_Measure_Election': elections[elections['date'] == infl['electionDate']]['name'].array[0],
+                'Filer_ID': f['registrations'].get('CA SOS'),
+                'Filer_NamL': infl['committeeName'],
+                '_Status': 'INACTIVE' if f['isTerminated'] else 'ACTIVE',
+                '_Committee_Type': (f['committeeTypes'][0]
+                                    if len(f['committeeTypes']) == 1
+                                    else 'Multiple Types'),
+                'Ballot_Measure': infl['measure'].get('measureNumber') if infl['measure'] else None,
+                'Support_Or_Oppose': infl['doesSupport'],
+                'Start_Date': infl['startDate'],
+                'End_Date': infl['endDate'],
+                'data_warning': None,
+                'Make_Active': None
+            } for f in filer_records
+            for infl in f['electionInfluences']
+        ])
