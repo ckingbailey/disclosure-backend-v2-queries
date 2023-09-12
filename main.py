4b63b34c008368c8f7109d088f1563a15871b38a
ckb <ckingbailey@gmail.com>
2023-09-11 21:00:48 -0700

    uncomment commit of elecs and committees

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 """ main, to run everything """
+from collections import Counter
+from datetime import datetime
 import json
+import pandas as pd
 from sqlalchemy import create_engine
 from model.a_contributions import A_Contributions
 from model.committee import CommitteeCollection
@@ -33,6 +36,7 @@ def main():
         filers = json.loads(f.read())
 
     committees = CommitteeCollection.from_filers(filers, elections).df
+    print('unique values of Support_Or_Oppose:', committees['Support_Or_Oppose'].unique().tolist())
 
     # A-Contribs:
     # join filers + filings + elections + transactions
@@ -44,9 +48,18 @@ def main():
         filings = FilingCollection(json.loads(f.read())).df
 
     with open('data/transactions.json', encoding='utf8') as f:
-        transactions = TransactionCollection(json.loads(f.read())).df
+        records = json.loads(f.read())
+        transactions = TransactionCollection(records)
 
-    a_contributions = A_Contributions(transactions, filings, committees, elections)
+        sup_opp_cd = [ t['Sup_Opp_Cd'] for t in transactions.data ]
+        print('sup_opp_cd in records', len(sup_opp_cd), set(sup_opp_cd))
+        print('ct of sup_opp_cd in records', Counter(sup_opp_cd))
+
+        transactions = transactions.df
+        print('sup_opp_cd in df', transactions['Sup_Opp_Cd'].unique().tolist())
+        print('ct of sup_opp_cd in df', transactions.groupby(by=['Sup_Opp_Cd'], dropna=False).size())
+
+    a_contributions = A_Contributions(transactions, filings, committees)
     a_contribs_df = a_contributions.df
     print(a_contribs_df.drop(columns=[
         'Form_Type',
@@ -56,23 +69,27 @@ def main():
         'XRef_Match',
         'Loan_Rate',
         'Int_CmteId',
+        'Memo_Code',
         'Memo_RefNo',
         'BakRef_TID',
         'Bal_Juris',
         'Bal_Num',
-        'Sup_Opp_Cd'
-    ]).head())
-    not_na_df = a_contribs_df[a_contribs_df['Sup_Opp_Cd'].notna()]
-    print(not_na_df.head())
-    print(a_contribs_df[['Rpt_Date','Tran_City', 'Tran_State']][a_contribs_df['Tran_City'].str.len() > 22])
+        'Juris_Cd',
+        'Juris_Dscr',
+        'Dist_No',
+        'Off_S_H_Cd',
+        'Bal_Name',
+        'tblDetlTran_Office_Cd',
+        'tblDetlTran_Offic_Dscr'
+    ]).sample(n=20))
 
     with engine.connect() as conn:
         common_opts = {
             'index_label': 'id',
             'if_exists': 'replace'
         }
-        # elections.to_sql('elections', conn, **common_opts)
-        # committees.drop(columns=['filer_nid']).to_sql('committees', conn, **common_opts)
+        elections.to_sql('elections', conn, **common_opts)
+        committees.drop(columns=['filer_nid']).to_sql('committees', conn, **common_opts)
         a_contributions.to_sql(conn, **common_opts)
 
 if __name__ == '__main__':
