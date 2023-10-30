a85e4d9057e6a4c2295df64f161b348e4815b6eb
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-10-30 08:21:10 -0700

    test pull from main

--- test_pull_from_gdrive.py
+++ test_pull_from_gdrive.py
@@ -1,4 +1,4 @@
 import os
 from gdrive_datastore.gdrive import test_data_pull
 
-test_data_pull(default_folder='OpenDisclosure')
+test_data_pull(default_folder='OpenDisclosure', default_subfolder='main')
